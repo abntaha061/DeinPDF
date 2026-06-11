0d9bbe3819670028cad371fa6b07b5752ba7e0a2
abntaha061 <abntaha061@gmail.com>
2026-06-11 10:22:28 +0300

    Update Color.kt

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -12,15 +12,17 @@ val Pink40 = Color(0xFF7D5260)
 
 // Custom App Design Theme Colors
 val DarkBg = Color(0xFF0A0C12)
+val DarkBackground = Color(0xFF000000) // أضفناه للثيم الأسود النقي
 val DarkSurface = Color(0xFF111320)
 val DarkCard = Color(0xFF161926)
 val DarkBorder = Color(0xFF1E2340)
+
 val AccentBlue = Color(0xFF3B82F6)
 val AccentPurple = Color(0xFF8B5CF6)
 val AccentCyan = Color(0xFF06B6D4)
 val Gold = Color(0xFFF59E0B)
 val ErrorRed = Color(0xFFEF4444)
 val SuccessGreen = Color(0xFF22C55E)
+
 val TextPrimary = Color(0xFFE2E8F0)
 val TextMuted = Color(0xFF64748B)
-
